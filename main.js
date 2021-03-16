69de5e1b9511830aff2a5428626342e0f9025cd1
Jack Taylor <77901413+jahreja@users.noreply.github.com>
2021-03-16 20:37:39 +0000

    more ds

--- main.js
+++ main.js
@@ -219,139 +219,297 @@
 
 ////////////////////////////////////////////////////////////////
 
-// Binary Search Tree
+// // Binary Search Tree
 
-class Node {
-    constructor(data, left = null, right = null){
-        this.data = data;
-        this.left = left;
-        this.right = right;
+// class Node {
+//     constructor(data, left = null, right = null){
+//         this.data = data;
+//         this.left = left;
+//         this.right = right;
+//     }
+// }
+
+// class BST {
+//     constructor(){
+//         this.root = null;
+//     }
+
+//     add(data){
+//         const node = this.root;
+//         if(node === null) {
+//             this.root = new Node(data);
+//             return;
+//         } else {
+//             const searchTree = function(node){
+//                 if (data < node.data) {
+//                     if (node.left === null) {
+//                         node.left = new Node(data);
+//                         return;
+//                     } else if (node.left !== null){
+//                         return searchTree(node.left);
+//                     }
+//                 } else if (data > node.data) {
+//                     if (node.right === null){
+//                         node.right = new Node(data);
+//                         return;
+//                     } else if (node.right !== null){
+//                         return searchTree(node.right);
+//                     }
+//                 } else {
+//                     return null;
+//                 }
+//             };
+//             return searchTree(node);
+//         }
+//     }
+
+//     findMin() {
+//         let current = this.root;
+//         while (current.left !== null){
+//             current = current.left;
+//         }
+//         return current.data;
+//     }
+
+//     findMax() {
+//         let current = this.root;
+//         while (current.right !== null){
+//             current = current.right;
+//         }
+//         return current.data;
+//     }
+
+//     find(data){
+//         let current = this.root;
+//         while (current.data !== this.data){
+//             if (data < current.data){
+//                 current = current.left;
+//             } else {
+//                 current = current.right;
+//             }
+//             if (current === null){
+//                 return null;
+//             }
+//         }
+//         return current; 
+//     }
+
+//     isPresent(data){
+//         let current = this.root;
+//         while (current) {
+//             if (data === current.data){
+//                 return true;
+//             }
+//             if (data < current.data){
+//                 current = current.left;
+//             } else {
+//                 current = current.right
+//             }
+//         }
+//         return false; 
+//     }
+
+//     remove(data) {
+//         const removeNode = function(node, data){
+//             if (node == null){
+//                 return null;
+//             }
+//             if (data == node.data) {
+//                 if (node.left == null && node.right == null){
+//                     return null;
+//                 }
+//                 if (node.left == null) {
+//                     return node.right;
+//                 }
+//                 if (node.right == null){
+//                     return node.left;
+//                 }
+//                 var tempNode = node.right;
+//                 while (tempNode.left !== null){
+//                     tempNode = tempNode.left;
+//                 }
+//                 node.data = tempNode.data;
+//                 node.right = removeNode(node.right, tempNode.data);
+//                 return node;
+//             } else if (data < node.data){
+//                 node.left = removeNode(node.left, data);
+//                 return node;
+//             } else {
+//                 node.right = removeNode(node.right, data);
+//                 return node;
+//             }
+//         }
+//         this.root = removeNode(this.root, data);
+//     }
+
+//     isBalanced(){
+//         return (this.findMinHeight() >= this.findMaxHeight() - 1)
+//     }
+
+//     findMinHeight(node = this.root){
+//         if (node == null){
+//             return -1;
+//         }
+//         let left = this.findMinHeight(node.left);
+//         let right = this.findMinHeight(node.right);
+//         if (left < right){
+//             return left + 1;
+//         } else {
+//             return right + 1;
+//         }
+//     }
+
+//     findMaxHeight(node = this.root){
+//         if (node == null){
+//             return -1;
+//         }
+//         if (node.left === null && node.right === null){
+//             return 0;
+//         }
+//         if (node.left === null){
+//             return this.findMaxHeight(node.right) + 1;
+//         }
+//         if (node.right === null){
+//             return this.findMaxHeight(node.left) + 1;
+//         }
+//         const lHeight = this.findMaxHeight(node.left);
+//         const rHeight = this.findMaxHeight(node.right);
+//         return Math.max(lHeight, rHeight) + 1;
+//     }
+
+// }
+
+// const bst = new BST();
+
+// bst.add(9);
+// bst.add(4);
+// bst.add(11);
+// bst.add(5);
+// bst.add(8);
+// bst.add(3);
+// bst.add(1);
+
+// console.log(bst.findMaxHeight());
+
+
+
+/////////////////////////////////////////////////////////////////////////
+
+// // Hash Tables
+
+// var hash = (string, max) => {
+//     var hash = 0;
+//     for (var i = 0; i < string.length; i++){
+//         hash += string.charCodeAt(i);
+//     }
+//     return hash % max;
+// }
+
+// let HashTable = function() {
+
+//     let storage = [];
+//     const storageLimit = 4;
+
+//     this.print = function() {
+//         console.log(storage)
+//     }
+
+//     this.add = function (key, value){
+//         var index = hash(key, storageLimit);
+//         if (storage[index] === undefined){
+//             storage[index] = [
+//                 [key, value]
+//             ];
+//         } else {
+//             var inserted = false;
+//             for (var i = 0; i < storage[index].length; i++){
+//                 if (storage[index][i][0] === key){
+//                     storage[index][i][1] = value;
+//                     inserted = true;
+//                 }
+//             }
+//             if (inserted === false){
+//                 storage[index].push([key, value]);
+//             }
+//         }
+//     }
+
+// }
+
+
+// Ben Awad Hash
+
+function hashStringToInt(s, tableSize) {
+    let hash = 17;
+
+    for (let i = 0; i < s.length; i++){
+        hash = (13 * hash * s.charCodeAt(i)) % tableSize
     }
+
+    return hash;
+
 }
 
-class BST {
-    constructor(){
-        this.root = null;
+class HashTable {
+
+    table = new Array(3);
+    numItems = 0;
+
+    resize = () => {
+        const newTable = new Array(this.table.length * 2);
+        this.table.forEach(item => {
+            if (item){
+                item.forEach(([key, value]) => {
+                    const index = hashStringToInt(key, newTable.length);
+                    if (newTable[index]){
+                        newTable[index].push([key, value])
+                    } else {
+                        newTable[index] = [[key, value]];    
+                    }
+                })
+            }
+        })
+        this.table = newTable;
     }
 
-    add(data){
-        const node = this.root;
-        if(node === null) {
-            this.root = new Node(data);
-            return;
+    setItem = (key, value) => {
+        this.numItems++;
+        const loadFactor = this.numItems / this.table.length;
+
+        if (loadFactor > .8){
+            console.log("resizing");
+            this.resize();
+        }
+
+        const index = hashStringToInt(key, this.table.length);
+        if (this.table[index]){
+            this.table[index].push([key, value])
         } else {
-            const searchTree = function(node){
-                if (data < node.data) {
-                    if (node.left === null) {
-                        node.left = new Node(data);
-                        return;
-                    } else if (node.left !== null){
-                        return searchTree(node.left);
-                    }
-                } else if (data > node.data) {
-                    if (node.right === null){
-                        node.right = new Node(data);
-                        return;
-                    } else if (node.right !== null){
-                        return searchTree(node.right);
-                    }
-                } else {
-                    return null;
-                }
-            };
-            return searchTree(node);
+            this.table[index] = [[key, value]];    
         }
+        
     }
 
-    findMin() {
-        let current = this.root;
-        while (current.left !== null){
-            current = current.left;
+    getItem = (key) => {
+        const index = hashStringToInt(key, this.table.length);
+        if (!this.table[index]){
+            return null;
         }
-        return current.data;
-    }
 
-    findMax() {
-        let current = this.root;
-        while (current.right !== null){
-            current = current.right;
-        }
-        return current.data;
-    }
-
-    find(data){
-        let current = this.root;
-        while (current.data !== this.data){
-            if (data < current.data){
-                current = current.left;
-            } else {
-                current = current.right;
-            }
-            if (current === null){
-                return null;
-            }
-        }
-        return current; 
-    }
-
-    isPresent(data){
-        let current = this.root;
-        while (current) {
-            if (data === current.data){
-                return true;
-            }
-            if (data < current.data){
-                current = current.left;
-            } else {
-                current = current.right
-            }
-        }
-        return false; 
-    }
-
-    remove(data) {
-        const removeNode = function(node, data){
-            if (node == null){
-                return null;
-            }
-            if (data == node.data) {
-                if (node.left == null && node.right == null){
-                    return null;
-                }
-                if (node.left == null) {
-                    return node.right;
-                }
-                if (node.right == null){
-                    return node.left;
-                }
-                var tempNode = node.right;
-                while (tempNode.left !== null){
-                    tempNode = tempNode.left;
-                }
-                node.data = tempNode.data;
-                node.right = removeNode(node.right, tempNode.data);
-                return node;
-            } else if (data < node.data){
-                node.left = removeNode(node.left, data);
-                return node;
-            } else {
-                node.right = removeNode(node.right, data);
-                return node;
-            }
-        }
-        this.root = removeNode(this.root, data);
+        return this.table[index].find(x => x[0] === key)[1];
     }
 }
 
-const bst = new BST();
+const myTable = new HashTable()
 
-bst.add(4);
-bst.add(2);
-bst.add(6);
-bst.add(1);
-bst.add(3);
-bst.add(5);
-bst.add(7);
-bst.remove(4);
+myTable.setItem("firstName", "jack");
+console.log(myTable.table.length);
+myTable.setItem("lastName", "taylor");
+myTable.setItem("age", 5);
+myTable.setItem("dob", "1/2/3");
+console.log(myTable.table.length);
+myTable.setItem("jkljkl", "taylor");
+myTable.setItem("kjlk", 5);
+myTable.setItem("iouu", "1/2/3");
+console.log(myTable.table.length);
 
-console.log(bst.isPresent(4));
